f3f4399c09a7ef4666c996bb32767360663f47b1
LUIS SILVA <luis.roberto.silva.guillen@gmail.com>
2020-05-28 22:50:22 -0400

    Start ignoring const and axios-order

--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: '[YOUR_API_ENDPINT_HERE]'
-})
-
-export default instance;
--- src/const.js
+++
@@ -1,7 +0,0 @@
-export const FIREBASE_API_KEY = '[YOUR_FIREBASE_API_KEY]'; 
- 
-export const FIREBASE_AUTH_BASE_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key='; 
-export const FIREBASE_AUTH_URL = FIREBASE_AUTH_BASE_URL + FIREBASE_API_KEY;
- 
-export const FIREBASE_AUTH_WITH_VERIFY_PASSWORD_BASE_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key='; 
-export const FIREBASE_AUTH_WITH_VERIFY_PASSWORD = FIREBASE_AUTH_WITH_VERIFY_PASSWORD_BASE_URL + FIREBASE_API_KEY;
